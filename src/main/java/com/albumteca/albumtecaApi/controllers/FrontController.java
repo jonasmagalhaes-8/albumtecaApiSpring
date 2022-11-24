643446e0c9e6195c6832afdcd8d7c7465aec8a62
Jonas Magalhães Silva <80786958+jonasmagalhaes-8@users.noreply.github.com>
2022-11-24 01:15:14 -0300

    Implementa integração do Fluter a Aplicação Spring

--- src/main/java/com/albumteca/albumtecaApi/controllers/FrontController.java
+++ src/main/java/com/albumteca/albumtecaApi/controllers/FrontController.java
@@ -1,12 +1,10 @@
 package com.albumteca.albumtecaApi.controllers;
 
 import java.util.Map;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/front")
+@Controller
 public class FrontController {
 
 	@GetMapping("/")
